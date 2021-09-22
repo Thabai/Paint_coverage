06f1f38949eacc0efc7b83df0616d575659d3a57
Carly Morris <thabai@hotmail.com>
2021-09-22 22:02:18 +0100

    objects room and windows & doors created

--- src/com/paint/Main.java
+++ src/com/paint/Main.java
@@ -9,70 +9,41 @@ public class Main {
     public static void main(String[] args) {
         double paintSqm;
         double paintPrice;
+        int wallNumber = 4;
         double myArea = 0;
-        double inputLength;
         double totalArea;
 
         Scanner PaintWantedCheck = new Scanner(System.in);
         System.out.println("Which paint would you like quoting for? (Choose 1-4)");
 
-        List<Paint> availablePaint = new ArrayList();
-        availablePaint.add(new Paint("Dulux Matt", 26.00, 10));
-        availablePaint.add(new Paint("Dulux Silk", 21.00, 18));
-        availablePaint.add(new Paint("Farrow & Ball Matt", 28.00, 15));
-        availablePaint.add(new Paint("Farrow & Ball Silk", 30.00, 25));
+        List<Paint> availablePaint = new ArrayList<>();
+        availablePaint.add(new Paint(0,"Default", 26.00, 10));
+        availablePaint.add(new Paint(1,"Dulux Matt", 26.00, 10));
+        availablePaint.add(new Paint(2,"Dulux Silk", 21.00, 18));
+        availablePaint.add(new Paint(3,"Farrow & Ball Matt", 28.00, 15));
+        availablePaint.add(new Paint(4,"Farrow & Ball Silk", 30.00, 25));
 
         for (Paint a: availablePaint) {
             System.out.println(a.printPaint());
         }
 
-        switch (PaintWantedCheck.nextInt()) {
-            case 1 -> {
-                System.out.println("You have chosen " + availablePaint.get(0).printPaint());
-                paintSqm = availablePaint.get(0).setCoverage();
-                paintPrice = availablePaint.get(0).setPrice();
-            }
-            case 2 -> {
-                System.out.println("You have chosen " + availablePaint.get(1).printPaint());
-                paintSqm = availablePaint.get(1).setCoverage();
-                paintPrice = availablePaint.get(1).setPrice();
-            }
-            case 3 -> {
-                System.out.println("You have chosen " + availablePaint.get(2).printPaint());
-                paintSqm = availablePaint.get(2).setCoverage();
-                paintPrice = availablePaint.get(2).setPrice();
-            }
-            case 4 -> {
-                System.out.println("You have chosen " + availablePaint.get(3).printPaint());
-                paintSqm = availablePaint.get(3).setCoverage();
-                paintPrice = availablePaint.get(3).setPrice();
-            }
-            default -> throw new IllegalStateException("Unexpected value: " + PaintWantedCheck.nextInt());
-        }
+        Paint chosenPaint = availablePaint.get(PaintWantedCheck.nextInt());
+        System.out.println("You have chosen " + chosenPaint.getName());
+        paintSqm = chosenPaint.getCoverage();
+        paintPrice = chosenPaint.getPrice();
 
         Scanner roomShapeCheck = new Scanner(System.in);
-        System.out.println("Do you have a standard shaped room? (true or false)");
+        System.out.println("Do you want to paint the whole room? (true or false)");
 
-        if (roomShapeCheck.nextBoolean()) {
-            Scanner myHeight = new Scanner(System.in);
-            System.out.println("Enter height of room in m");
-            Scanner myLength = new Scanner(System.in);
-            System.out.println("Enter length of room in m");
-            Scanner myWidth = new Scanner(System.in);
-            System.out.println("Enter width of room in m");
-            myArea = getRegularArea(myHeight, myLength, myWidth);
-        } else {
+        if (!roomShapeCheck.nextBoolean()) {
+            Scanner wallNumberCheck= new Scanner(System.in);
+            System.out.println("Enter number of walls");
+            wallNumber = wallNumberCheck.nextInt();
+            }
+
             Scanner wallHeight = new Scanner(System.in);
             System.out.println("Enter height of walls");
-            double inputHeight = wallHeight.nextDouble();
-
-            for (int i = 1; i <= 4; i++) {
-                Scanner wallLengths = new Scanner(System.in);
-                System.out.println("Enter length of wall number " + i);
-                inputLength = wallLengths.nextDouble();
-                myArea += inputLength * inputHeight;
-            }
-        }
+            myArea = getMyArea(wallNumber, myArea, wallHeight);
 
         Scanner windowDoorCheck = new Scanner(System.in);
         System.out.println("Does the room have any windows or doors? (true or false)");
@@ -81,8 +52,8 @@ public class Main {
             Scanner windowDoorLength = new Scanner(System.in);
             System.out.println("What is the total length of all windows and doors?");
             Scanner windowDoorWidth = new Scanner(System.in);
-            System.out.println("What is the total height of all windows and doors?");
-            totalArea = windowDoorArea(myArea, windowDoorLength, windowDoorWidth);
+            System.out.println("What is the total width of all windows and doors?");
+            totalArea = getTotalArea(myArea, windowDoorLength, windowDoorWidth);
         } else {
             totalArea = myArea;
         }
@@ -97,32 +68,34 @@ public class Main {
         double paintCheck= (totalArea/paintSqm);
         int paintAreaCheck = (int) Math.ceil(paintCheck);
         double costCheck = paintAreaCheck * paintPrice;
-//        double usedPaint = (paintCheck % (paintSqm/ myArea));
         System.out.println("Area of room is " + totalArea + "sqm");
         System.out.println("You will need " + paintAreaCheck + " pots of 1L paint");
         System.out.println("It will cost £" + costCheck + " to complete this job");
-//        System.out.println("You will use " + usedPaint + " L of paint");
     }
 
-    private static double getRegularArea(Scanner myHeight, Scanner myLength, Scanner myWidth) {
-        double myArea;
-        double roomLength;
-        double roomHeight;
-        double roomWidth;
-        roomHeight = myHeight.nextDouble();
-        roomLength = (myLength.nextDouble() * 2) * roomHeight;
-        roomWidth = (myWidth.nextDouble() * 2) * roomHeight;
-        myArea = roomLength + roomWidth;
+    private static double getMyArea(int wallNumber, double myArea, Scanner wallHeight) {
+        Room room;
+        room = new Room();
+        room.setHeight(wallHeight.nextDouble());
+
+        for (int i = 1; i <= wallNumber; i++) {
+            Scanner wallLengths = new Scanner(System.in);
+            System.out.println("Enter length of wall number " + i);
+            room.setLength(wallLengths.nextDouble());
+            myArea += room.getArea();
+        }
         return myArea;
     }
 
-    private static double windowDoorArea(double myArea, Scanner windowDoorLength, Scanner windowDoorWidth) {
+    private static double getTotalArea(double myArea, Scanner windowDoorLength, Scanner windowDoorWidth) {
         double totalArea;
-        double emptyLength = windowDoorLength.nextDouble();
-        double emptyWidth = windowDoorWidth.nextDouble();
-        double emptyArea = emptyLength * emptyWidth;
-        totalArea = myArea - emptyArea;
+        WindowsDoors space;
+        space = new WindowsDoors();
+        space.setWDLength(windowDoorLength.nextDouble());
+        space.setWDWidth(windowDoorWidth.nextDouble());
+        totalArea = myArea - space.getWDArea();
         return totalArea;
     }
+
 
 }
